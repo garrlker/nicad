40d7290287dc5c1533dfafaf3d3fd6b4e127abe4
Garrett Walker <garbwalk@gmail.com>
2019-12-22 09:02:08 -0600

    Started work on the `Feature` component. This will be a wrapper component around the `vue-csg` library. It lets you feed it a node-child array of CSG definitions and renders out the tree dynamically into a model. The current implementation works with 3D-Primitives and Booleans. Now we need a `Feature-Tree` component to create the tree on the fly to drive nicad

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
 <template>
   <div id="app">
-    <input type="number" v-model.number="test">
+    <input type="number" v-model.number="test" />
     <viewer ref="viewer" />
-    <scale @csg="setGeometry" :xform="10">
-      <center :xform="[true, true, true]">
-        <translate :xform="[0, 0, 1.5]">
-          <union>
-            <subtract>
-              <cube :size="test" :center="true"/>
-              <sphere :r="2" :center="true"/>
-            </subtract>
-            <intersect>
-              <sphere :r="1.3" :center="true"/>
-              <cube :size="2.1" :center="true"/>
-            </intersect>
-          </union>
-        </translate>
-      </center>
-    </scale>
-    <!-- <cube :size="test" @csg="setGeometry">
-    </cube> -->
-    <!-- <cube :pos="[0.5,0.5,0.5]"/> -->
+    <feature :node="tree" @csg="setGeometry" />
   </div>
 </template>
 
 <script>
 import Viewer from "./components/Viewer.vue";
-import FakeValue from "./components/FakeValue.vue";
+import Feature from "./components/Feature.vue";
 import * as VueCSG from "vue-csg";
-console.log("DEBUG", VueCSG);
 const debug = require("debug")("App");
 import { CAG, CSG } from "@jscad/csg";
 
@@ -38,19 +19,6 @@ import { primitives3d } from "@jscad/scad-api";
 
 Object.assign(window, booleanOps);
 Object.assign(window, primitives3d);
-
-/*
-  return union(
-    difference(
-      cube({size: 3, center: true}),
-      sphere({r: 2, center: true})
-    ),
-    intersection(
-      sphere({r: 1.3, center: true}),
-      cube({size: 2.1, center: true})
-    )
-  ).translate([0, 0, 1.5]).scale(10);
-*/
 
 // import Worker from "./lib/worker.worker.js";
 // const worker = new Worker();
@@ -66,48 +34,77 @@ export default {
   name: "app",
   components: {
     Viewer,
-    FakeValue,
+    Feature,
     ...VueCSG
   },
   data() {
     return {
       arr: new Array(2),
       foo: [],
-      size: 10,
-      test: 3
+      test: 3,
+      tree: {
+        name: "union",
+        id: 0,
+        props: {},
+        children: [
+          {
+            name: "subtract",
+            id: 1,
+            props: {},
+            children: [
+              {
+                name: "cube",
+                id: 2,
+                props: {
+                  size: 3,
+                  center: true
+                }
+              },
+              {
+                name: "sphere",
+                id: 3,
+                props: {
+                  r: 2,
+                  center: true
+                }
+              }
+            ]
+          },
+          {
+            name: "intersect",
+            id: 2,
+            props: {},
+            children: [
+              {
+                name: "sphere",
+                id: 3,
+                props: {
+                  r: 1.3,
+                  center: true
+                }
+              },
+              {
+                name: "cube",
+                id: 2,
+                props: {
+                  size: 2.1,
+                  center: true
+                }
+              }
+            ]
+          }
+        ]
+      }
     };
   },
   methods: {
     setGeometry(geometry) {
       debug("Set Geometry", geometry);
-      console.log("GEOMETRY", geometry);
-      // while(this.$refs.viewer.$scene.children.length){
-      //   this.$refs.viewer.$scene.children.remove(this.$refs.viewer.$scene.children[0]);
-      // }
-      // this.$refs.viewer.$scene = [];
       this.$refs.viewer.setGeometry(geometry);
-    },
-    updateSize(size) {
-      this.size = size;
     }
   },
   mounted() {
     debug("CSG", CSG);
-
-    // for (var i = 1; i <= 10; i++) {
-    // this.foo.push(i);
-    // }
-    // let cube1 = CSG.cube({});
-    // worker.postMessage(cube1);
-    // debug(cube1);
-    // let cube2 = CSG.cube({center: [0.5,0.5,0]});
-    // let finalShape = cube1['intersect'](cube2);
-    let finalShape = difference(
-      cube({ size: 2.1, center: true }),
-      cube({ size: 2.1, center: true }).translate([1, 1, 1])
-    );
-
-    // this.setGeometry(finalShape);
   }
 };
 </script>
---
+++ src/components/Feature.vue
@@ -0,0 +1,29 @@
+<template>
+  <translate :xform="node.props.translate || [0,0,0]" v-on="$listeners">
+    <scale :xform="node.props.scale || [1,1,1]">
+      <rotate :xform="node.props.rotate || [0,0,0]">
+        <component :is="node.name" v-bind="node.props">
+          <div v-if="node.children">
+            <feature v-for="childNode in node.children" :key="childNode.id" :node="childNode"/>
+          </div>
+        </component>
+      </rotate>
+    </scale>
+  </translate>
+</template>
+
+<script>
+import * as VueCSG from "vue-csg";
+export default {
+  name: "Feature",
+  props:{
+    node:{
+      type: Object,
+      default: () => {}
+    }
+  },
+  components: {
+    ...VueCSG
+  }
+};
+</script>
