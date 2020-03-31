0556b04ab009306bbaffba952186a71d8cc1243d
Garrett <garbwalk@gmail.com>
2020-03-30 22:30:04 -0500

    Working on touch screen camera controls. Zooming is now working. Zooming to the mouse cursor is working on desktop. Need to get panning working on mobile. HammerJS is being tricky about how it leaves/starts events

--- src/js/camera.js
+++ src/js/camera.js
@@ -1,30 +1,25 @@
-var mouseChange = require('mouse-change')
-var mouseWheel = require('mouse-wheel')
+var mouseChange = require("mouse-change");
+var mouseWheel = require("mouse-wheel");
 import Hammer from "hammerjs";
 
-import {
-  mat4,
-  vec3,
-  quat
-} from "gl-matrix";
+import { mat4, vec3, quat } from "gl-matrix";
 
-var {
-  setAxisAngle
-} = quat;
+var { setAxisAngle } = quat;
 
 function createCamera(regl, props_) {
   var canvas = regl._gl.canvas;
-  var props = props_ || {}
+  var props = props_ || {};
   var eye = [0, 0, 100];
-  var rotationQuat = [0, 0, 0, 0];  
-  var modelCenter = [0, 0, 0] //TODO: Models can have non 0,0,0 centers, this is hardcoded for now. Should be prop in future
+  var rotationQuat = [0, 0, 0, 0];
+  var modelCenter = [0, 0, 0]; //TODO: Models can have non 0,0,0 centers, this is hardcoded for now. Should be prop in future
 
   // Camera Settings
   var zoomScale = 0.8;
 
   // Internal
   var prevX, prevY;
-  var touchPrevX = 0, touchPrevY = 0;
+  var touchPrevX = 0,
+    touchPrevY = 0;
   var right = new Float32Array(3);
   var panUp = new Float32Array(3);
 
@@ -36,56 +31,73 @@ function createCamera(regl, props_) {
     up: new Float32Array(props.up || [0, 1, 0]),
     right: new Float32Array([1, 0, 0]),
     fovy: props.fovy || Math.PI / 4.0,
-    near: typeof props.near !== 'undefined' ? props.near : -1000,
-    far: typeof props.far !== 'undefined' ? props.far : 1000.0,
+    near: typeof props.near !== "undefined" ? props.near : -1000,
+    far: typeof props.far !== "undefined" ? props.far : 1000.0,
     frustumWidth: 100,
     frustumHeight: 100,
-    rotationSpeed: typeof props.rotationSpeed !== 'undefined' ? props.rotationSpeed : 1,
-    zoomSpeed: typeof props.zoomSpeed !== 'undefined' ? props.zoomSpeed : 1,
-    renderOnDirty: typeof props.renderOnDirty !== undefined ? !!props.renderOnDirty : false
-  }
+    rotationSpeed:
+      typeof props.rotationSpeed !== "undefined" ? props.rotationSpeed : 1,
+    zoomSpeed: typeof props.zoomSpeed !== "undefined" ? props.zoomSpeed : 1,
+    renderOnDirty:
+      typeof props.renderOnDirty !== undefined ? !!props.renderOnDirty : false
+  };
 
   var aspect = getWidth() / getHeight();
   cameraState.frustumWidth *= aspect;
   cameraState.frustumHeight = cameraState.frustumWidth * (1 / aspect);
 
   function getWidth() {
-    return canvas ? canvas.offsetWidth : window.innerWidth
+    return canvas ? canvas.offsetWidth : window.innerWidth;
   }
 
   function getHeight() {
-    return canvas ? canvas.offsetHeight : window.innerHeight
+    return canvas ? canvas.offsetHeight : window.innerHeight;
   }
 
   /* Mouse Handling */
-  mouseChange(canvas, function (buttons, x, y) {
+  mouseChange(canvas, function(buttons, x, y) {
     if (buttons & 1) {
-      var dx = (x - prevX) / getWidth()
-      var dy = (y - prevY) / getHeight()
+      var dx = (x - prevX) / getWidth();
+      var dy = (y - prevY) / getHeight();
 
       rotateCamera(dx, dy);
     }
 
     if (buttons & 4) {
-      var dx = (x - prevX) / getWidth()
-      var dy = (y - prevY) / getHeight()
+      var dx = (x - prevX) / getWidth();
+      var dy = (y - prevY) / getHeight();
 
       panCamera(dx, dy);
     }
 
-    prevX = x
-    prevY = y
-  })
+    prevX = x;
+    prevY = y;
+  });
 
-  mouseWheel(canvas, function (dx, dy) {
-    zoomCamera(dy);
-  }, true)
+  mouseWheel(
+    canvas,
+    function(_dx, scrollDelta, _dz, ev) {
+      var x = ev.clientX;
+      var y = ev.clientY;
+      var centerX = getWidth() / 2;
+      var centerY = getHeight() / 2;
+
+      var dx = (centerX - x) / getWidth();
+      var dy = (centerY - y) / getHeight();
+
+      panCamera(dx, dy);
+      zoomCamera(scrollDelta);
+      panCamera(-dx, -dy);
+    },
+    true
+  );
 
   /* Touch Handling */
-  var hammer = Hammer(canvas, {});
+  var mc = new Hammer(canvas, {});
+  mc.get("pan").set({ direction: Hammer.DIRECTION_ALL, pointers: 2 });
 
-  hammer.on('panstart', function(ev) {
-    if(ev.pointerType === "mouse"){
+  mc.on("panstart", function(ev) {
+    if (ev.pointerType === "mouse") {
       return;
     }
 
@@ -94,60 +106,78 @@ function createCamera(regl, props_) {
 
     touchPrevX = ev.deltaX;
     touchPrevY = ev.deltaY;
-    console.log('panstart', touchPrevX, touchPrevY);
+    console.log("panstart", touchPrevX, touchPrevY);
   });
 
-  hammer.on('pan', function(ev) {
-    // console.log(ev);
-    if(ev.pointerType === "mouse"){
+  mc.on("panleft panright panup pandown", function(ev) {
+    console.log(ev);
+    if (ev.pointerType === "mouse") {
       return;
     }
 
     var dx = (ev.deltaX - touchPrevX) / getWidth();
     var dy = (ev.deltaY - touchPrevY) / getHeight();
 
-    rotateCamera(dx, dy);
+    if (ev.pointers.length === 1) {
+      rotateCamera(dx, dy);
+    } else if (ev.pointers.length === 2) {
+      panCamera(dx, dy);
+    }
     // console.log("Touch Deltas", dx, dy);
     touchPrevX = ev.deltaX;
     touchPrevY = ev.deltaY;
     console.log("pan", touchPrevX, touchPrevY);
   });
 
+  var prevScale;
+  var pinch = new Hammer.Pinch();
+  var rotate = new Hammer.Rotate();
+
+  // we want to detect both the same time
+  pinch.recognizeWith(rotate);
+  mc.add([pinch, rotate]);
+  mc.on("pinch rotate", function(ev) {
+    if(prevScale){
+      zoomCamera(1 + (ev.scale - prevScale));
+    }
+    prevScale = ev.scale;
+    console.log("PINCH ROTATE", ev);
+  });
 
   /* Camera Functions */
-  function rotateCamera(xDelta, yDelta){
+  function rotateCamera(xDelta, yDelta) {
     let difference = [0, 0, 0];
     vec3.sub(difference, cameraState.target, modelCenter);
 
-    if(xDelta !== 0){
+    if (xDelta !== 0) {
       setAxisAngle(rotationQuat, cameraState.up, -xDelta * 5);
       vec3.transformQuat(cameraState.eye, cameraState.eye, rotationQuat);
       vec3.transformQuat(cameraState.right, cameraState.right, rotationQuat);
       vec3.transformQuat(difference, difference, rotationQuat);
     }
 
-    if(yDelta !== 0){
+    if (yDelta !== 0) {
       setAxisAngle(rotationQuat, cameraState.right, -yDelta * 5);
       vec3.transformQuat(cameraState.eye, cameraState.eye, rotationQuat);
       vec3.transformQuat(cameraState.up, cameraState.up, rotationQuat);
       vec3.transformQuat(difference, difference, rotationQuat);
     }
 
-    vec3.add(difference, difference, modelCenter)
+    vec3.add(difference, difference, modelCenter);
     vec3.copy(cameraState.target, difference);
   }
 
-  function panCamera(xDelta, yDelta){
-    if(xDelta !== 0){
+  function panCamera(xDelta, yDelta) {
+    if (xDelta !== 0) {
       let normalEye = new Float32Array(3);
       vec3.normalize(normalEye, cameraState.eye);
       vec3.cross(right, normalEye, cameraState.up);
-      vec3.scale(right, right, xDelta * cameraState.frustumWidth );
+      vec3.scale(right, right, xDelta * cameraState.frustumWidth);
       vec3.add(cameraState.eye, cameraState.eye, right);
       vec3.add(cameraState.target, cameraState.target, right);
     }
 
-    if(yDelta !== 0){
+    if (yDelta !== 0) {
       let normalEye = new Float32Array(3);
       vec3.normalize(normalEye, cameraState.eye);
 
@@ -159,7 +189,7 @@ function createCamera(regl, props_) {
     }
   }
 
-  function zoomCamera(yDelta){
+  function zoomCamera(zoomScale) {
     // Perspective
     // if (yDelta < 0) {
     //   vec3.scale(cameraState.eye, cameraState.eye, zoomScale);
@@ -168,13 +198,13 @@ function createCamera(regl, props_) {
     // }
 
     // Orthographic
-    if (yDelta < 0) {
+    // if (yDelta < 0) {
       cameraState.frustumWidth *= zoomScale;
       cameraState.frustumHeight *= zoomScale;
-    } else {
-      cameraState.frustumWidth *= 1 / zoomScale;
-      cameraState.frustumHeight *= 1 / zoomScale;
-    }
+    // } else {
+      // cameraState.frustumWidth *= 1 / zoomScale;
+      // cameraState.frustumHeight *= 1 / zoomScale;
+    // }
   }
 
   /*        perspective(cameraState.projection,
@@ -186,42 +216,47 @@ function createCamera(regl, props_) {
 
   var injectContext = regl({
     context: Object.assign({}, cameraState, {
-      dirty: function () {
+      dirty: function() {
         return cameraState.dirty;
       },
-      projection: function (context) {
+      projection: function(context) {
         mat4.ortho(
           cameraState.projection,
-          -( cameraState.frustumWidth / 2),
-          ( cameraState.frustumWidth / 2),
-          -( cameraState.frustumHeight / 2 ),
-          ( cameraState.frustumHeight / 2 ),
+          -(cameraState.frustumWidth / 2),
+          cameraState.frustumWidth / 2,
+          -(cameraState.frustumHeight / 2),
+          cameraState.frustumHeight / 2,
           cameraState.near,
-          cameraState.far  
-        )
-        return cameraState.projection
+          cameraState.far
+        );
+        return cameraState.projection;
       }
     }),
-    uniforms: Object.keys(cameraState).reduce(function (uniforms, name) {
-      uniforms[name] = regl.context(name)
-      return uniforms
+    uniforms: Object.keys(cameraState).reduce(function(uniforms, name) {
+      uniforms[name] = regl.context(name);
+      return uniforms;
     }, {})
-  })
+  });
 
   function setupCamera(props, block) {
     // If no regl block argument was passed, then props is probably the regl block
     if (!block) {
-      block = props
-      props = {}
+      block = props;
+      props = {};
     }
 
-    mat4.lookAt(cameraState.view, cameraState.eye, cameraState.target, cameraState.up)
+    mat4.lookAt(
+      cameraState.view,
+      cameraState.eye,
+      cameraState.target,
+      cameraState.up
+    );
 
     // updateCamera(props)
-    injectContext(block)
+    injectContext(block);
   }
 
-  return setupCamera
+  return setupCamera;
 }
 
 export default createCamera;
